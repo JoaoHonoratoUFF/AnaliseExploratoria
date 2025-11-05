1e423638d0fec8388c623202297586ea89da277f
Joao Victor <joaohonorato@id.uff.br>
2025-11-05 10:47:06 -0300

    Script da análise inicial.

---
+++ Script_Analise_V1.R
@@ -0,0 +1,42 @@
+# Análise ----
+
+## Pacotes ----
+
+library(readxl)
+
+## Base ----
+
+Base_trabalho <- read_excel("Base_trabalho.xlsx")
+
+## Dicionário ----
+
+Dicionario_Base_Trabalho <- read_excel("dicionario_Base_trabalho.xlsx")
+
+
+## Convertendo as var. qualitativas em fatores ----
+
+str(Base_trabalho)
+
+Base_trabalho$escolaridade <- as.factor(Base_trabalho$escolaridade)
+Base_trabalho$reincidente  <- as.factor(Base_trabalho$reincidente)
+Base_trabalho$filhos       <- as.factor(Base_trabalho$filhos)
+Base_trabalho$sexo         <- as.factor(Base_trabalho$sexo)
+Base_trabalho$casado       <- as.factor(Base_trabalho$casado)
+
+## Verificando NAs ----
+
+sum(is.na(Base_trabalho))
+
+## Histograma da var. "Idade" ----
+
+# Histograma da idade
+
+hist(Base_trabalho$idade,
+     main = "Histograma da Idade",
+     xlab = "Idade do detento (anos completos)",
+     ylab = "Frequência",
+     col = "grey",
+     border = "black",
+     breaks = 15,       
+     xlim = c(min(Base_trabalho$idade, na.rm = TRUE),
+              max(Base_trabalho$idade, na.rm = TRUE)))    
